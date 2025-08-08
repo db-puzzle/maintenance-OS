f2bd6ede3c9b36a8b410025fd72380996c43c65a
Danilo Bibancos <danilo.bibancos@gmail.com>
2025-08-08 18:41:58 -0300

    rremoved ziggy.js

--- resources/js/ziggy.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from 'ziggy-js';
-
-declare module '@/ziggy' {
-    export const Ziggy: Config & { location: string };
-    export { route } from 'ziggy-js';
-    export type RouteName = string;
-}
